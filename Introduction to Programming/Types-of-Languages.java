22c257671a3473af98dad9cc390bf9c86f3873ae
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-06 10:08:21 +0530

    types of language

--- Introduction to Programming/Types-of-Languages.java
+++ Introduction to Programming/Types-of-Languages.java
@@ -5,3 +5,16 @@
 // #1. procedural
 // #2. functional
 // #3. object oriented
+
+
+// Procedural
+// -Specifies a series of well structure steps and procedure to compose 
+// a program.
+
+// Functional
+// -wrinting a program only in pure function. i.e never modify variables but only create new one as ouput.
+
+// Object oriented
+// -resolve around object
+// -Dat + Code = Object
+// developed to make it eaiser to reuse code and maintain software.
