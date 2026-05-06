4b00b6b6d18b82d7f4fa6f4fe67f2657ac42c760
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-06 10:59:45 +0530

    memeory management

--- Introduction to Programming/mermory management.java
+++ Introduction to Programming/mermory management.java
@@ -1,98 +1,98 @@
-/*
-========================================
-        Memory Management in Java
-========================================
+// /*
+// ========================================
+//         Memory Management in Java
+// ========================================
 
-Java automatically memory manage karta hai.
-Programmer ko manually memory free nahi karni padti.
+// Java automatically memory manage karta hai.
+// Programmer ko manually memory free nahi karni padti.
 
-Java me mainly 3 important concepts hote hain:
+// Java me mainly 3 important concepts hote hain:
 
-1. Stack Memory
-2. Heap Memory
-3. Garbage Collector
-*/
-
-
-// Student class
-class Student {
-
-    // Instance Variable
-    int age = 20;
-}
+// 1. Stack Memory
+// 2. Heap Memory
+// 3. Garbage Collector
+// */
 
 
-public class Main {
+// // Student class
+// class Student {
 
-    public static void main(String[] args) {
-
-        /*
-        ========================================
-                STACK MEMORY
-        ========================================
-
-        - Local variables stack memory me store hote hain.
-        - Method call bhi stack me jata hai.
-        - Fast memory hoti hai.
-        - Method finish hone par automatically remove ho jati hai.
-        */
-
-        int x = 10;
-
-        System.out.println("Value of x : " + x);
+//     // Instance Variable
+//     int age = 20;
+// }
 
 
+// public class Main {
+// // 
+//     public static void main(String[] args) {
 
-        /*
-        ========================================
-                HEAP MEMORY
-        ========================================
+//         /*
+//         ========================================
+//                 STACK MEMORY
+//         ========================================
 
-        - Objects heap memory me bante hain.
-        - "new" keyword object create karta hai.
-        - Object ke andar ka data heap me store hota hai.
-        */
+//         - Local variables stack memory me store hote hain.
+//         - Method call bhi stack me jata hai.
+//         - Fast memory hoti hai.
+//         - Method finish hone par automatically remove ho jati hai.
+//         */
 
-        Student s1 = new Student();
+//         int x = 10;
 
-        System.out.println("Student Age : " + s1.age);
+//         System.out.println("Value of x : " + x);
 
 
 
-        /*
-        ========================================
-            GARBAGE COLLECTOR (GC)
-        ========================================
+//         /*
+//         ========================================
+//                 HEAP MEMORY
+//         ========================================
 
-        - Java automatically unused objects ko delete karta hai.
-        - Is process ko Garbage Collection bolte hain.
-        - Programmer ko free() ya delete() use nahi karna padta.
-        */
+//         - Objects heap memory me bante hain.
+//         - "new" keyword object create karta hai.
+//         - Object ke andar ka data heap me store hota hai.
+//         */
 
-        s1 = null;
+//         Student s1 = new Student();
 
-        System.out.println("Object is now eligible for Garbage Collection");
+//         System.out.println("Student Age : " + s1.age);
 
 
 
-        /*
-        ========================================
-                MEMORY FLOW
-        ========================================
+//         /*
+//         ========================================
+//             GARBAGE COLLECTOR (GC)
+//         ========================================
 
-        Stack Memory:
-        x
-        s1 (reference variable)
+//         - Java automatically unused objects ko delete karta hai.
+//         - Is process ko Garbage Collection bolte hain.
+//         - Programmer ko free() ya delete() use nahi karna padta.
+//         */
 
-        Heap Memory:
-        Student Object
-        age = 20
+//         s1 = null;
 
-        Jab:
-        s1 = null;
+//         System.out.println("Object is now eligible for Garbage Collection");
 
-        tab object ka koi reference nahi bachta.
-        Isliye Garbage Collector future me us object ko remove kar dega.
-        */
-    }
-}
+
+
+//         /*
+//         ========================================
+//                 MEMORY FLOW
+//         ========================================
+
+//         Stack Memory:
+//         x
+//         s1 (reference variable)
+
+//         Heap Memory:
+//         Student Object
+//         age = 20
+
+//         Jab:
+//         s1 = null;
+
+//         tab object ka koi reference nahi bachta.
+//         Isliye Garbage Collector future me us object ko remove kar dega.
+//         */
+//     }
+// }
