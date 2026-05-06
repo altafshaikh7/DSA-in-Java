f4f571ba2bff4b973f34913e49be870f7c229873
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-06 10:24:10 +0530

    Static-vs-Dynamic-language

---
+++ Introduction to Programming/Static-vs-Dynamic-language.java
@@ -0,0 +1,28 @@
+// Static vs Dynamic Programming Languages
+
+
+
+// 1. Static Language
+// - Variable ka data type pehle define karna padta hai.
+// - Type checking compile time par hoti hai.
+// - Errors jaldi mil jati hain.
+// - Fast aur secure hoti hai.
+
+// Example:
+//  int age = 20;
+
+// Languages:
+// Java, C, C++
+
+
+
+// 2. Dynamic Language
+// - Variable ka type automatically decide hota hai.
+// - Type checking runtime par hoti hai.
+// - Flexible hoti hai but runtime errors aa sakte hain.
+
+// Example:
+// age = 20
+
+// Languages:
+// Python, JavaScript
